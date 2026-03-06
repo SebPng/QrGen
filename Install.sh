a22a7524d0224866c0b1f1449b82bc1089401547
Seb <153007942+SebPng@users.noreply.github.com>
2026-03-07 00:05:45 +0100

    Test

--- Install.sh
+++ Install.sh
@@ -6,13 +6,14 @@ mkdir -p "$INSTALL_DIR"
 TARGET="$INSTALL_DIR/qrgen"
 
 echo "Downloading QRGen CLI..."
-RAW_URL="https://raw.githubusercontent.com/SebPng/QRGen/main/qrgen.py"
 
-# Download and rename to qrgen
+# Use your raw file URL here
+RAW_URL="https://raw.githubusercontent.com/SebPng/QrGen/refs/heads/main/qrgen.py?token=GHSAT0AAAAAADW5SYCC6NGQA2URH73D6W6U2NLLXKQ"
+
 if curl -L -o "$TARGET" "$RAW_URL"; then
     echo "Download successful!"
 else
-    echo "Failed to download QRGen. Check your internet or URL."
+    echo "❌ Failed to download QRGen. Check your internet or URL."
     exit 1
 fi
 
@@ -24,6 +25,6 @@ if ! grep -q "$INSTALL_DIR" "$HOME/.bashrc"; then
     echo "Added $INSTALL_DIR to PATH. Restart terminal or run: source ~/.bashrc"
 fi
 
-echo "Installation complete!"
+echo "✅ Installation complete!"
 echo "You can now run QRGen CLI like this:"
 echo "   qrgen 'Hello world!' --multi 2"
